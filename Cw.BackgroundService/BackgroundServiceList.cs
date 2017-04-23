ea4a0773f0660a89b81a6ae2dbc9b7d6a3b78632
U+ <cool-whispers@hotmail.com>
2017-04-23 22:58:56 +0800

    加入專案檔案。

---
+++ Cw.BackgroundService/BackgroundServiceList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cw.BackgroundService
+{
+    public class BackgroundServiceList
+    {
+        internal static BackgroundServiceList Create()
+        {
+            return new BackgroundServiceList();
+        }
+
+        private BackgroundServiceList()
+        {
+
+        }
+
+        private List<Guid> _serivceIds = new List<Guid>();
+
+        public void Add(IBackgroundService service)
+        {
+            BackgroundService.Add(service);
+        }
+
+        public async Task AddAsync(IBackgroundService service)
+        {
+            await Task.Run(() => Add(service));
+        }
+
+        public void Stop()
+        {
+            var tasks = new List<Task>();
+
+            foreach (var id in _serivceIds)
+            {
+                tasks.Add(BackgroundService.StopAsync(id));
+            }
+
+            foreach (var task in tasks)
+            {
+                task.Wait();
+            }
+        }
+
+    }
+}
---
+++ Cw.BackgroundService/IBackgroundService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cw.BackgroundService
+{
+    public interface IBackgroundService
+    {
+        /// <summary>
+        /// Background Start
+        /// </summary>
+        void BackgroundStart();
+    }
+}
