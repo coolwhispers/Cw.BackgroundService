2640b4035bbe87ec8132b286314338df2ac116b0
coolwhispers <cool-whispers@hotmail.com>
2017-06-05 17:18:28 +0800

    add ScheduleCondition

--- Cw.BackgroundService/Schedule.cs
+++ Cw.BackgroundService/Schedule.cs
@@ -5,197 +5,122 @@ using System.Threading.Tasks;
 namespace Cw.BackgroundService
 {
     /// <summary>
-    /// 排程物件介面
-    /// </summary>
-    public interface ISchedule
-    {
-        /// <summary>
-        /// 開始執行
-        /// </summary>
-        void Start();
-        /// <summary>
-        /// 要求停止
-        /// </summary>
-        void Stop();
-        /// <summary>
-        /// 要求停止
-        /// </summary>
-        Task StopAsync();
-    }
-
-    /// <summary>
     /// 排程服務
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    public sealed class Schedule<T> : ISchedule where T : IBackgroundProcess
+    public sealed class Schedule<T> where T : IBackgroundProcess
     {
-        /// <summary>
-        /// 排程模式
-        /// </summary>
-        private enum ScheduleMode
-        {
-            /// <summary>
-            /// 自訂
-            /// </summary>
-            Custom,
+        private delegate bool Condition(DateTime lastProcessTime);
 
-            /// <summary>
-            /// 間隔幾秒
-            /// </summary>
-            Interval,
-
-            /// <summary>
-            /// 每天
-            /// </summary>
-            Daily,
-
-            /// <summary>
-            /// 每週
-            /// </summary>
-            Weekly,
-
-            /// <summary>
-            /// 每月
-            /// </summary>
-            Monthly,
-        }
-
-        private ScheduleMode _scheduleMode;
-
-        private int _interval;
-
-        /// <summary>
-        /// 間隔時間執行
-        /// </summary>
-        /// <param name="interval">The interval.</param>
-        public Schedule(int interval)
-        {
-            _scheduleMode = ScheduleMode.Interval;
-            _interval = interval;
-        }
-
-        private int _hours;
-        private int _minutes;
-
-        /// <summary>
-        /// 每天執行
-        /// </summary>
-        /// <param name="hours">The hours.</param>
-        /// <param name="minutes">The minutes.</param>
-        public Schedule(int hours, int minutes)
-        {
-            _scheduleMode = ScheduleMode.Daily;
-            _hours = hours;
-            _minutes = minutes;
-        }
-
-        private delegate bool CustomSchedule();
-
-        private CustomSchedule _customSchedule;
+        private Condition _scheduleCondition;
 
         /// <summary>
         /// 自訂執行條件式
         /// </summary>
-        /// <param name="func">The function.</param>
-        public Schedule(Func<bool> func)
+        /// <param name="scheduleCondition">執行條件式(in DateTime LastProcessTime, out bool IfExecute)</param>
+        public Schedule(Func<DateTime, bool> scheduleCondition)
         {
-            _scheduleMode = ScheduleMode.Custom;
-            _customSchedule = new CustomSchedule(func);
+            _isRun = false;
+            _isComplete = false;
+            _isExecute = false;
+
+            _scheduleCondition = new Condition(scheduleCondition);
+
+            LoadLastProcessTime();
         }
 
-        private int _day;
-
-        /// <summary>
-        /// 每月
-        /// </summary>
-        /// <param name="day">The day.</param>
-        /// <param name="hours">The hours.</param>
-        /// <param name="minutes">The minutes.</param>
-        public Schedule(int day, int hours, int minutes)
+        private void LoadLastProcessTime()
         {
-            _scheduleMode = ScheduleMode.Monthly;
-            _day = day;
-            _hours = hours;
-            _minutes = minutes;
+            LastProcessTime = DateTime.MinValue;
         }
 
-        private DayOfWeek _dayOfWeek;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Schedule{T}"/> class.
-        /// </summary>
-        /// <param name="dayOfWeek">The day of week.</param>
-        /// <param name="hours">The hours.</param>
-        /// <param name="minutes">The minutes.</param>
-        public Schedule(DayOfWeek dayOfWeek, int hours, int minutes)
+        private void SaveLastProcessTime()
         {
-            _scheduleMode = ScheduleMode.Weekly;
-            _dayOfWeek = dayOfWeek;
-            _hours = hours;
-            _minutes = minutes;
+            LastProcessTime = DateTime.Now;
         }
 
-        private bool _isStop;
+        private bool _isRun;
 
         private bool _isComplete;
 
-        private bool _isRuning;
+        private bool _isExecute;
 
         private Thread _thread;
-
-        private DateTime _lastProcessTime;
 
         /// <summary>
         /// 最後執行時間
         /// </summary>
-        public DateTime LastProcessTime => _lastProcessTime;
+        public DateTime LastProcessTime { get; private set; }
+
+        /// <summary>
+        /// 排程最後存活時間
+        /// </summary>
+        public DateTime LastAliveTime { get; private set; }
+
+        private void Execute()
+        {
+            try
+            {
+                _isExecute = true;
+
+                var instance = Activator.CreateInstance<T>();
+
+                instance.BackgroundStart();
+
+                if (instance is IDisposable)
+                {
+                    ((IDisposable)instance).Dispose();
+                }
+            }
+            catch (Exception)
+            {
+            }
+            finally
+            {
+                SaveLastProcessTime();
+
+                _isExecute = false;
+            }
+        }
 
         /// <summary>
         /// 執行
         /// </summary>
         internal void Process()
         {
-            _isStop = false;
-            _isComplete = false;
-            _isRuning = false;
-
             do
             {
-                if (RunCondition())
+                LastAliveTime = DateTime.Now;
+
+                if (_scheduleCondition.Invoke(LastProcessTime))
                 {
-                    var instance = Activator.CreateInstance<T>();
-
-                    _isRuning = true;
-                    try
-                    {
-                        instance.BackgroundStart();
-
-                        if (instance is IDisposable)
-                        {
-                            ((IDisposable)instance).Dispose();
-                        }
-
-                        _isRuning = false;
-
-                        _lastProcessTime = DateTime.UtcNow;
-
-                        GC.Collect();
-                    }
-                    catch (Exception)
-                    {
-                    }
+                    Execute();
                 }
+
+                GC.Collect();
             }
-            while (!_isStop);
+            while (_isRun);
 
             _isComplete = true;
         }
+
+        private object startLock = new object();
 
         /// <summary>
         /// 開始執行
         /// </summary>
         public void Start()
         {
+            lock (startLock)
+            {
+                if (_isRun == true)
+                {
+                    return;
+                }
+
+                _isRun = true;
+            }
+
             _thread = new Thread(new ThreadStart(Process));
 
             _thread.Start();
@@ -204,17 +129,22 @@ namespace Cw.BackgroundService
         /// <summary>
         /// 要求停止
         /// </summary>
-        public void Stop()
+        /// <param name="abort">強制停止</param>
+        public void Stop(bool abort = false)
         {
-            _isStop = true;
+            _isRun = false;
 
             while (!_isComplete)
             {
-                if (!_isRuning)
+                if (abort || !_isExecute)
                 {
-                    _isRuning = false;
                     _thread.Abort();
+                    _isExecute = false;
                     _isComplete = true;
+                }
+                else
+                {
+                    Thread.Sleep(3000);
                 }
             }
         }
@@ -227,133 +157,128 @@ namespace Cw.BackgroundService
         {
             await Task.Run(() => Stop());
         }
+    }
 
-        /// <summary>
-        /// 執行條件式
-        /// </summary>
-        /// <returns></returns>
-        private bool RunCondition()
+    /// <summary>
+    /// 執行條件
+    /// </summary>
+    public sealed class ScheduleCondition
+    {
+        private ScheduleCondition()
         {
-            switch (_scheduleMode)
-            {
-                case ScheduleMode.Interval:
-                    return IntervalSchedule();
-
-                case ScheduleMode.Daily:
-                    return DailySchedule();
-
-                case ScheduleMode.Weekly:
-                    return WeeklySchedule();
-
-                case ScheduleMode.Monthly:
-                    return MonthlySchedule();
-
-                case ScheduleMode.Custom:
-                    return _customSchedule.Invoke();
-            }
-
-            _isStop = true;
-            return false;
         }
 
-        private bool IntervalSchedule()
+        private static void Sleep(DateTime nextTime, DateTime now)
         {
-            var seconds = (LastProcessTime.AddSeconds(_interval) - DateTime.UtcNow).TotalSeconds;
-
-            var half = _interval / 2;
-
-            if (seconds > half && seconds > 10)
+            if (now.CompareTo(nextTime) >= 0)
             {
-                Thread.Sleep(10000);
-            }
-            else if (seconds > 1)
-            {
-                Thread.Sleep(1000);
-            }
-            else if (seconds > 0)
-            {
-                Thread.Sleep(Convert.ToInt32(seconds * 1000));
+                return;
             }
 
-            return (LastProcessTime.AddSeconds(_interval) - DateTime.UtcNow).TotalSeconds < 0;
-        }
-
-        private bool DailySchedule()
-        {
-            var now = DateTime.UtcNow;
-            var nextTime = now.Date.AddHours(_hours).AddMinutes(_minutes);
-            var compare = now.CompareTo(nextTime);
-            if (compare > 0)
+            var sleepTimeSpan = nextTime - now;
+            if (sleepTimeSpan.TotalMinutes > 1)
             {
-                Thread.Sleep(nextTime.AddDays(1) - now);
-            }
-            else if (compare < 0)
-            {
-                Thread.Sleep(now - nextTime);
-            }
-
-            return true;
-        }
-
-        private bool WeeklySchedule()
-        {
-            var now = DateTime.UtcNow;
-            var nextTime = now.Date.AddHours(_hours).AddMinutes(_minutes);
-
-            var compare = now.CompareTo(nextTime);
-            while (compare < 0 && nextTime.DayOfWeek != _dayOfWeek)
-            {
-                nextTime = nextTime.AddDays(1);
+                Thread.Sleep(15000);
+                return;
             }
 
             Thread.Sleep(nextTime - now);
-
-            return true;
         }
 
-        private bool MonthlySchedule()
+        private static bool IsExpired(DateTime nextTime)
         {
-            var now = DateTime.UtcNow;
-            var nextTime = new DateTime(now.Year, now.Month, _day, _hours, _minutes, 0);
+            return DateTime.Now.CompareTo(nextTime) <= 0;
+        }
 
-            var compare = now.CompareTo(nextTime);
-            while (compare < 0 && nextTime.DayOfWeek != _dayOfWeek)
-            {
-                nextTime = nextTime.AddDays(1);
-            }
+        private static bool ReturnValue(DateTime nextTime, DateTime now)
+        {
+            Sleep(nextTime, now);
 
-            Thread.Sleep(nextTime - now);
-
-            return true;
+            return IsExpired(nextTime);
         }
 
         /// <summary>
-        /// 儲存執行條件
+        /// 固定間格執行
         /// </summary>
-        private void SaveConfig(string str)
-        {
-            var typeNmae = this.GetType().Name;
-
-            System.IO.File.WriteAllText(string.Format("{0}.sche", typeNmae), str);
-        }
-
-        /// <summary>
-        /// 取得執行條件
-        /// </summary>
+        /// <param name="seconds">間格秒數</param>
         /// <returns></returns>
-        private string GetConfig()
+        public static Func<DateTime, bool> Interval(int seconds)
         {
-            var typeNmae = this.GetType().Name;
+            return new Func<DateTime, bool>(lastProcessTime =>
+             {
+                 var now = DateTime.Now;
+                 var nextTime = lastProcessTime.AddSeconds(seconds);
 
-            try
-            {
-                return System.IO.File.ReadAllText(string.Format("{0}.sche", typeNmae));
-            }
-            catch (Exception)
-            {
-            }
+                 return ReturnValue(nextTime, now);
+             });
+        }
 
-            return null;
+        /// <summary>
+        /// 每天執行
+        /// </summary>
+        /// <param name="hours">小時</param>
+        /// <param name="minutes">分鐘</param>
+        /// <returns></returns>
+        public static Func<DateTime, bool> Daily(int hours, int minutes)
+        {
+            return new Func<DateTime, bool>(x =>
+            {
+                var now = DateTime.Now;
+                var nextTime = now.Date.AddHours(hours).AddMinutes(minutes);
+                var compare = now.CompareTo(nextTime);
+
+                return ReturnValue(nextTime, now);
+            });
+        }
+
+        /// <summary>
+        /// 每週執行
+        /// </summary>
+        /// <param name="dayOfWeek">星期</param>
+        /// <param name="hours">小時</param>
+        /// <param name="minutes">分鐘</param>
+        /// <returns></returns>
+        public static Func<DateTime, bool> Weekly(DayOfWeek dayOfWeek, int hours, int minutes)
+        {
+            return new Func<DateTime, bool>(lastProcessTime =>
+            {
+                var now = DateTime.Now;
+                var nextTime = now.Date.AddHours(hours).AddMinutes(minutes);
+
+                if (now.CompareTo(nextTime) < 0)
+                {
+                    while (nextTime.DayOfWeek != dayOfWeek || nextTime.Date == lastProcessTime.Date)
+                    {
+                        nextTime = nextTime.AddDays(1);
+                    }
+                }
+
+                return ReturnValue(nextTime, now);
+            });
+        }
+
+        /// <summary>
+        /// 每月執行
+        /// </summary>
+        /// <param name="day">日期</param>
+        /// <param name="hours">小時</param>
+        /// <param name="minutes">分鐘</param>
+        /// <returns></returns>
+        public static Func<DateTime, bool> Monthly(int day, int hours, int minutes)
+        {
+            return new Func<DateTime, bool>(lastProcessTime =>
+            {
+                var now = DateTime.Now;
+                var lastProcessDay = lastProcessTime.Date;
+                var nextTime = new DateTime(now.Year, now.Month, day, hours, minutes, 0);
+
+                if (now.CompareTo(nextTime) < 0 && lastProcessDay.Date == nextTime.Date)
+                {
+                    nextTime = nextTime.AddMonths(1);
+                }
+
+                return ReturnValue(nextTime, now);
+            });
         }
     }
 }
